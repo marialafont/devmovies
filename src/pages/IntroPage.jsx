315335d4a785a80cb99ad5cead3c77dcbc01442b
marialafont <merlafont@gmail.com>
2025-09-10 15:37:54 -0300

    feat: add responsive design to IntroPage

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -20,16 +20,20 @@ export function IntroPage() {
         }}
       />
 
-      <div className="relative z-20 flex flex-col items-center gap-12">
+      <div className="relative z-20 flex flex-col items-center gap-8 md:gap-12 px-4">
         <div>
-          <Logo width="450px" height="auto" className="mx-auto" />
+          <Logo
+            width="320px"
+            height="auto"
+            className="mx-auto md:w-[400px] lg:w-[450px]"
+          />
         </div>
 
         <Button
           onClick={() => navigate('/home')}
           variant="white"
           size="custom"
-          className="px-10 py-3 text-xl tracking-[0.1em]"
+          className="px-8 py-2 text-lg md:px-10 md:py-3 md:text-xl tracking-[0.1em]"
         >
           ENTER
         </Button>
