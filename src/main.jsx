f0fdeaa897b1bd4561f2e4bdc97ccac9f8635969
marialafont <merlafont@gmail.com>
2025-09-13 17:45:53 -0300

    optimize main.jsx for production deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { queryClient } from '@/config'
 import { MovieProvider } from '@/context'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <MovieProvider>
-          <App />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </MovieProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <MovieProvider>
+        <App />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </MovieProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
 )
